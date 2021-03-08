811d3361d927a48a485b239ff05cdba3549929f8
rowanlee92 <watrv41@gmail.com>
2021-03-08 23:41:35 +0900

    Add: Sort Testing

---
+++ src/main/java/com/janjanee/infthejavatest/SelectionSortTest.java
@@ -0,0 +1,38 @@
+package com.janjanee.infthejavatest;
+
+import java.util.Arrays;
+
+public class SelectionSortTest {
+
+    static void swap(int[] arr, int idx1, int idx2) {
+        int tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
+    }
+
+    static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            swap(arr, i, min);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 12, 11, 40, 8 , 13, 20};
+
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
