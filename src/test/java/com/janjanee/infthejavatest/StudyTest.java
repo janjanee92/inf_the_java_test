a25751efd03bde5d1adced8461491ec7601708af
rowanlee92 <watrv41@gmail.com>
2021-04-06 00:03:29 +0900

    Add: 4/5

--- src/test/java/com/janjanee/infthejavatest/StudyTest.java
+++ src/test/java/com/janjanee/infthejavatest/StudyTest.java
@@ -2,19 +2,73 @@ package com.janjanee.infthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
-    @Test
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
-    void test_1() {
-        System.out.println("I'm MAC!");
+    int value = 1;
+
+    @Order(2)
+    @FastTest
+    void create_new_study() {
+        Study actual = new Study(100);
+        System.out.println(value++);
+        assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
-    @Test
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "janjanee")
-    void test_2() {
-        System.out.println("I'm Janjanee");
+    @Order(1)
+    @SlowTest
+    void create_new_study_again() {
+        System.out.println("create1");
+        System.out.println(value++);
+    }
+
+    @Order(4)
+    @Disabled
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" +  repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @Order(3)
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0} ")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
 }
