4791615fa5f454e4061052bf9ac91ec869642a7f
rowanlee92 <watrv41@gmail.com>
2021-04-15 10:05:51 +0900

    Fix: typo

--- src/test/java/com/janjanee/infthejavatest/study/StudyServiceTest.java
+++ src/test/java/com/janjanee/infthejavatest/study/StudyServiceTest.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
